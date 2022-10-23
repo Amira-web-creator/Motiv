d2018dbb94b1f5d71dd1a3055f5045f0a0c007fc
unknown <amiramaherramez@gmail.com>
2022-10-23 09:50:49 +0200

    progressbar modifications

--- src/myComponents/utils/Item.jsx
+++ src/myComponents/utils/Item.jsx
@@ -1,23 +1,16 @@
 import React from "react";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
-import "./custom.css";
+import "./Custom.css";
 
-const Item = ({
-  id,
-  color,
-  title,
-  img,
-  hoverColor,
-  percentage,
-}) => {
+const Item = ({ id, color, title, img, hoverColor, percentage }) => {
   return (
     <>
       <div
-        className={`relative text-black hover:text-white  ${color} ${hoverColor} grid items-center justify-items-center rounded-xl  px-2 transition-all duration-700 ease-in-out w-full hover:scale-105`}
+        className={` relative text-black hover:text-white  ${color} ${hoverColor} range-block grid items-center justify-items-center rounded-xl  px-2 transition-all duration-700 ease-in-out w-full hover:scale-105`}
       >
-        <div className="flex flex-col items-center justify-items-center">
-          <div className="bg-slate-200 rounded-full my-3 w-10 h-10 flex items-center justify-center">
+        <div className="flex flex-col items-center justify-items-center ">
+          <div className="bg-slate-200 rounded-full my-3 w-10 h-10 flex items-center justify-center ">
             <img
               src={img}
               alt={`img/item-img/${id}`}
@@ -30,7 +23,7 @@ const Item = ({
           </h1>
 
           <div className="flex item-center gap-1 my-5 ">
-            <h1 className="md:text-sm w-14  font-normal">
+            <h1 className="md:text-sm w-20 font-normal">
               <CircularProgressbar
                 className="m-auto "
                 value={percentage}
